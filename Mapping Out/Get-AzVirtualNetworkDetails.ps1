7e90cbb5ea39f6cdffb1f2c6ad8529d063a3405f
wesellis <wes@wesellis.com>
2023-04-06 01:02:09 -0400

    Create Get-AzVirtualNetworkDetails.ps1

---
+++ Mapping Out/Get-AzVirtualNetworkDetails.ps1
@@ -0,0 +1,28 @@
+# Connect to Azure account
+Connect-AzAccount
+
+# Get list of all VNETs in subscription
+$vnetList = Get-AzVirtualNetwork
+
+# Create an array to store VNET details
+$vnetDetails = @()
+
+# Loop through each VNET and add details to array
+ForEach ($vnet in $vnetList) {
+    $subnetNames = $vnet.Subnets | ForEach-Object { $_.Name }
+    $subnetNames = $subnetNames -join "; "
+    
+    $peeringNames = $vnet.VirtualNetworkPeerings | ForEach-Object { $_.Name }
+    $peeringNames = $peeringNames -join "; "
+    
+    $vnetDetails += [PSCustomObject]@{
+        Name = $vnet.Name
+        ResourceGroupName = $vnet.ResourceGroupName
+        AddressPrefixes = ($vnet.AddressSpace.AddressPrefixes -join "; ")
+        Subnets = $subnetNames
+        PeeringNames = $peeringNames
+    }
+}
+
+# Export VNET details to CSV file
+$vnetDetails | Export-Csv -Path "C:\VNETDetails.csv" -NoTypeInformation
